f25ce3043cc88794d7edd38db76850712922fdee
JuanCarlosBrenes <juancarlosbrenes27@gmail.com>
2025-08-15 20:27:28 -0600

    CI pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,12 +3,19 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   timeout: 30 * 1000,
+  reporter: [
+    ['dot'],
+    ['junit', { outputFile: 'test-results/junit/results.xml'}],
+    ['html', { open: 'never', outputFolder: 'playwright-report'}]
+  ],
   use: {
+    baseURL: 'http://localhost:3000',
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
+    trace: 'on-first-retry',
   },
   projects: [
     {
